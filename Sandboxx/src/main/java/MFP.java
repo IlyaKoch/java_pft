09d446cb6d1b328a3738085ecf13921db587e8b8
IlyaKoch <kochetkov6363@gmail.com>
2020-06-24 13:08:39 +0400

    Изменения настроек

--- Sandboxx/src/main/java/MFP.java
+++
@@ -1,7 +0,0 @@
-public class MFP {
-
-  public static void main(String[] args) {
-   System.out.println("HI, WORLD!");
- }
-
-}
---
+++ Sandboxx/src/main/java/ru/stqa/pft/sandbox/MFP.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+public class MFP {
+
+  public static void main(String[] args) {
+    System.out.println("HI, WORLD!");
+  }
+
+}
